72951c08d0f9b37bf8f5bacf38c47313e4c8e1c0
Dmitry <dmitry.ivoninsky@gmail.com>
2021-04-02 09:35:09 +0300

    dublication code

--- api/src/main/java/com/epam/esm/GiftApplication.java
+++ api/src/main/java/com/epam/esm/GiftApplication.java
@@ -13,9 +13,28 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EntityScan(basePackages = {"com.epam.esm.model.entity"})
 public class GiftApplication {
     public static void main(String[] args) {
-        String testString = null;
-        testString.contains("123");
+        testMethod("qwe");
 
         SpringApplication.run(GiftApplication.class, args);
+    }
+
+    private static void testMethod(String a) {
+        boolean qwe = a.contains("qwe");
+        System.out.println(qwe);
+    }
+
+    private static void testMethod2(String a) {
+        boolean qwe = a.contains("qwe");
+        System.out.println(qwe);
+    }
+
+    private static void testMethod3(String a) {
+        boolean qwe = a.contains("qwe");
+        System.out.println(qwe);
+    }
+
+    private static void testMethod4(String a) {
+        boolean qwe = a.contains("qwe");
+        System.out.println(qwe);
     }
 }
