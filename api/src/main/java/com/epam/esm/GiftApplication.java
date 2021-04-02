b55c66ba4de04c76a170b514d4cb535816af2198
Dmitry <dmitry.ivoninsky@gmail.com>
2021-04-02 11:27:09 +0300

    minor fixes

--- api/src/main/java/com/epam/esm/GiftApplication.java
+++ api/src/main/java/com/epam/esm/GiftApplication.java
@@ -6,34 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-
 @EnableResourceServer
 @EnableAuthorizationServer
 @SpringBootApplication
 @EntityScan(basePackages = {"com.epam.esm.model.entity"})
 public class GiftApplication {
     public static void main(String[] args) {
-//        testMethod("qwe");
         SpringApplication.run(GiftApplication.class, args);
     }
-
-//    private static void testMethod(String a) {
-//        boolean qwe = a.contains("qwe");
-//        System.out.println(qwe);
-//    }
-//
-//    private static void testMethod2(String a) {
-//        boolean qwe = a.contains("qwe");
-//        System.out.println(qwe);
-//    }
-//
-//    private static void testMethod3(String a) {
-//        boolean qwe = a.contains("qwe");
-//        System.out.println(qwe);
-//    }
-//
-//    private static void testMethod4(String a) {
-//        boolean qwe = a.contains("qwe");
-//        System.out.println(qwe);
-//    }
 }
