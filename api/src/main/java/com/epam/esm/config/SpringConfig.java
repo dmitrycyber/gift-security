940f71545f66d7b023b1d6774a97fbba2e6794f9
Dmitry <dmitry.ivoninsky@gmail.com>
2021-04-02 09:44:22 +0300

    remove unused import

--- api/src/main/java/com/epam/esm/config/SpringConfig.java
+++ api/src/main/java/com/epam/esm/config/SpringConfig.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.hateoas.client.LinkDiscoverer;
